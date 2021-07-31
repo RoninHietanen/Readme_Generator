882f5bc362316651e14f8a6f9934eea29126b9bb
RoninHietanen <84194778+RoninHietanen@users.noreply.github.com>
2021-08-01 00:57:52 +1000

    added to the index.js and the generateMarkdown.js files

--- index.js
+++ index.js
@@ -1,13 +1,106 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'Please enter the name of your webpage.',
+        name: 'webname',
+    },
+    {
+        type: 'input',
+        message: 'Please write a brief discription about your project.',
+        name: 'discription',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the AS A part of your user story.',
+        name: 'asA',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the I WANT part of your user story.',
+        name: 'iWant',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your SO THAT I part of your user story.',
+        name: 'soThatI',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your acceptance criteria.',
+        name: 'acceptance',
+    },
+    {
+        type: 'input',
+        message: 'For the mock up portion please enter weather it is an animation or a screenshot of your webpage.',
+        name: 'whatis',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the pathway in your file for the animation or screenshot. eg -- "./assests/images/Covid19Tracker.gif".',
+        name: 'giforpic'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the URL for your live webpage.',
+        name: 'livelink'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the first key technology used.',
+        name: 'first'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the discription of the first ket technology used.',
+        name: 'firstdiscription'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the second key technology used.',
+        name: 'second'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the discription of the second ket technology used.',
+        name: 'seconddiscription'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the third key technology used.',
+        name: 'third'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the discription of the third ket technology used.',
+        name: 'thirddiscription'
+    },
+  ])
+  .then((data) => {
+    const htmlPageContent = generateHTML(data);
+    // TODO: Create a function to write README file
+    fs.writeFile('README.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+
+
+
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,54 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.webname}
 
-`;
+  ## Description
+  md
+  ${data.discription}
+
+
+  ## User Story 
+
+  md
+  As A ${data.asA} 
+  I WANT ${data.iWant}
+  SO THAT I ${data.soThatI}
+
+
+  ## Acceptance Criteria 
+
+  md
+  ${data.acceptance}
+
+
+  ## Mock up
+
+  The following ${data.whatis} shows the web application's appearance and functionality:
+
+  ![${data.webname}](./assests/images/${data.giforpic})
+
+  You can view the deployed webpage at this link. [${data.webname}](${data.livelink}).
+
+  ## Key Technologies Used
+
+  **${data.first}**
+
+  ${data.firstdiscription}
+
+  **${data.second}**
+
+  ${data.seconddiscription}
+
+  **${data.third}**
+
+  ${data.thirddiscription}`;
+
+  //{
+  //    type: 'input',
+  //    message: 'Please enter .',
+  //    name: ' '
+  //},
 }
 
 module.exports = generateMarkdown;
